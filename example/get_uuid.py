7c3e794be23f7fea7395438090678a73470ad96a
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2023-05-30 12:24:39 +0800

    📃 docs(example): switch to YggdrasilPlayer

--- example/get_uuid.py
+++ example/get_uuid.py
@@ -1,9 +1,10 @@
-from yggdrasil_mc import YggPlayerUuid, YggPlayerProfile
+from yggdrasil_mc import YggdrasilPlayer
 
 player_name = "w84"
-player = YggPlayerUuid().getMojang(player_name)
+ygg = YggdrasilPlayer()
+player = ygg.Uuid.getMojang(player_name)
 if player.existed:
-    print(YggPlayerProfile().getMojang(player.id))
+    print(ygg.Profile.getMojang(player.id))
 
 # OUTPUT >>>
 # id='ca244462f8e5494791ec98f0ccf505ac' name='w84' properties=Properties(...
